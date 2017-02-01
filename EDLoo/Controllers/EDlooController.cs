d901326a9848953619f809ffcc6307cc1bc1f5fa
Gomes Christopher <gomchrist@gmail.com>
2017-02-01 19:00:48 +0530

    Add project files.

---
+++ EDLoo/Controllers/EDlooController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDLoo.Controllers
+{
+    public class EDlooController : Controller
+    {
+        // GET: EDloo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult POC1()
+        {
+            return View();
+        }
+        public ActionResult School()
+        {
+            return View();
+        }
+        public ActionResult College()
+        {
+            return View();
+        }
+    }
+}
